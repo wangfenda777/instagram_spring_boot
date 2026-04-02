c698e5fa99a60deb347f5e1feaee0184a5c6ebe2
qiwang83 <qiwang83@iflytek.com>
2026-04-02 20:23:50 +0800

    初始化项目

--- src/main/java/com/example/usermanage/mapper/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.usermanage.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.example.usermanage.entity.User;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface UserMapper extends BaseMapper<User> {
-}
